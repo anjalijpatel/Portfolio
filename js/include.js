eb852b817a4834f4ff48b43acabd5a260b55bbe0
Anjali Patel <anjalijpatel000@gmail.com>
2025-05-31 12:48:08 +0530

    updated styles

--- js/include.js
+++ js/include.js
@@ -1,20 +1,20 @@
 // js/include.js
 function includeHTML(id, file, callback) {
   fetch(file)
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById(id).innerHTML = data;
       if (callback) callback();
     })
-    .catch(error => console.error('Error loading ' + file + ':', error));
+    .catch((error) => console.error("Error loading " + file + ":", error));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  includeHTML("include-header", "/Portfolio/components/header.html", function() {
+  includeHTML("include-header", "components/header.html", function () {
     // Now that header (and canvas) is loaded, load script.js
-    var s = document.createElement('script');
-    s.src = '/Portfolio/js/script.js';
+    var s = document.createElement("script");
+    s.src = "js/script.js";
     document.body.appendChild(s);
   });
-  includeHTML("include-footer", "/Portfolio/components/footer.html");
+  includeHTML("include-footer", "components/footer.html");
 });
